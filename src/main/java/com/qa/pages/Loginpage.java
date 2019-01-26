fc90d75a9ca9ddf1b4e0cc0e3329ba8dda764680
Rajubhai234 <47036232+Rajubhai234@users.noreply.github.com>
2019-01-26 11:45:28 +0530

    first commit

---
+++ src/main/java/com/qa/pages/Loginpage.java
@@ -0,0 +1,20 @@
+package com.qa.pages;
+
+public class Loginpage {
+	
+	
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("print loginpage credentials");
+		
+		
+	}
+	
+	
+	
+
+
+
+}
---
+++ src/test/java/com/qa/test/Loginpage.java
@@ -0,0 +1,19 @@
+package com.qa.test;
+
+import org.testng.annotations.Test;
+
+public class Loginpage {
+	
+	
+	
+	@Test
+	public void test1() {
+		System.out.println("print test1");
+		
+	}
+	
+	public void test21() {
+		System.out.println("test23");
+	}
+
+}
